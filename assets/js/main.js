70871df55fe2910d24672e3ea292140d66f52bde
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-05-09 18:54:01 -0300

    Desarrollo de botón printAll(), el cual imprime en pantalla todas las estudiantes que se han ido agregando con addStudent()

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-hola bebe
+function printAll(){
+    var miDiv = document.getElementById("mostrar-pantalla");
+    miDiv.innerHTML = "";
+    estudiantes.forEach(function(value){
+    miDiv.innerHTML += "<br>" + "Nombre: " + value.nombre + "<br>" + "Porcentaje Técnico: " + value.porcentajeTecnico + "%" + "<br>" + "Porcentaje HSE: " + value.porcentajeHSCStudent + "%" + "<br>";
+    }); 
+}
