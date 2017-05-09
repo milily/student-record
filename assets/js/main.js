5eb3508b1eaa4c35f48edff025071ab9c8cf9e3c
marcelaorellana <marcelapaz.1985@gmail.com>
2017-05-09 19:33:16 -0300

    se agregó el constructor y la funcion addStudent

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-hola bebe
+var estudiantes = [];
+
+//Constructor:
+function Students(nombre, porcentajeTecnico, porcentajeHSC){
+    this.nombre = nombre;
+    this.porcentajeTecnico = porcentajeTecnico;
+    this.porcentajeHSC = porcentajeHSC;
+}
+
+function addStudent(){
+    var nombreStudent = prompt("Nombre de Estudiante:");
+    var porcentajeTecnicoStudent = prompt("Porcentaje Técnico:");
+    var porcentajeHSCStudent = prompt("Porcentaje Habilidades Socio Emocionales:");
+    var miDiv = document.getElementById("mostrar-pantalla");
+
+    var estudiante = new Students(nombreStudent, porcentajeTecnicoStudent, porcentajeHSCStudent);
+    estudiantes.push(estudiante);
+    miDiv.innerHTML = "Nombre: " + nombreStudent + "<br>Porcentaje Técnico: " + porcentajeTecnicoStudent + "<br>Porcentaje HSC: " + porcentajeHSCStudent;
+}
