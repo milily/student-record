e84f291cf45389509f4a020cab13e58925f585af
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-05-09 19:12:43 -0300

    Se corrige el nombre de la variable value.porcentajeHSCStudent, en realidad se debe poner value.porcentajeHSC

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,6 @@ function printAll(){
     var miDiv = document.getElementById("mostrar-pantalla");
     miDiv.innerHTML = "";
     estudiantes.forEach(function(value){
-    miDiv.innerHTML += "<br>" + "Nombre: " + value.nombre + "<br>" + "Porcentaje Técnico: " + value.porcentajeTecnico + "%" + "<br>" + "Porcentaje HSE: " + value.porcentajeHSCStudent + "%" + "<br>";
+    miDiv.innerHTML += "<br>" + "Nombre: " + value.nombre + "<br>" + "Porcentaje Técnico: " + value.porcentajeTecnico + "%" + "<br>" + "Porcentaje HSE: " + value.porcentajeHSC + "%" + "<br>";
     }); 
 }
